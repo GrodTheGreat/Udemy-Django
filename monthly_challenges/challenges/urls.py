cb00d51b871331b575e76fa31fde299daa7131c4
Gerard Causse <grod.causse@gmail.com>
2025-03-30 19:10:12 -0400

    Complete Including Partial Template Snippets lesson

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # * ORDER MATTERS!!!
-    path(route="", view=views.index),
+    path(route="", view=views.index, name="index"),
     path(route="<int:month>", view=views.monthly_challenge_by_number),
     path(route="<str:month>", view=views.monthly_challenge, name="month-challenge"),
 ]
