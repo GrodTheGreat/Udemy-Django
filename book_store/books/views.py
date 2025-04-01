2e323f87a234bf8e7563a341cab108086cdf73e9
Gerard Causse <grod.causse@gmail.com>
2025-04-01 13:04:57 -0400

    Complete Aggregation & Ordering lesson

--- book_store/books/views.py
+++ book_store/books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
@@ -6,11 +7,17 @@ from .models import Book
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    average_rating = books.aggregate(Avg("rating"))
     return render(
         request=request,
         template_name="books/index.html",
-        context={"books": books},
+        context={
+            "books": books,
+            "total_number_of_books": num_books,
+            "average_rating": average_rating,
+        },
     )
 
 
