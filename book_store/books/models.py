7ae801e5a64bdd749ae919a0a3e3ce5699e2c715
Gerard Causse <grod.causse@gmail.com>
2025-04-01 12:50:57 -0400

    Complete Adding a Slugfield & Overwriting Save lesson

--- book_store/books/models.py
+++ book_store/books/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -11,9 +12,15 @@ class Book(models.Model):
     )
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
 
     def get_absolute_url(self):
-        return reverse("book-detail", kwargs={"id": self.pk})
+        return reverse(viewname="book-detail", kwargs={"id": self.pk})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(value=self.title)
+        #! super().save() is a requirement!
+        return super().save(*args, **kwargs)
