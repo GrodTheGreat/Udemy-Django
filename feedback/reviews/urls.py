3669903f8c70e450bd3691130bd9c0d908171aff
Gerard Causse <grod.causse@gmail.com>
2025-04-02 16:25:44 -0400

    Complete Handling Form Submission & Extracting Data lesson

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path(route="", view=views.review)]
+urlpatterns = [
+    path(route="", view=views.review, name="index"),
+    path(route="thank-you", view=views.thank_you),
+]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,16 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
 def review(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        entered_name = request.POST.get("name")
+        print(entered_name)
+        return HttpResponseRedirect("/thank-you")
+
     return render(request=request, template_name="reviews/review.html")
+
+
+def thank_you(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="reviews/thank-you.html")
