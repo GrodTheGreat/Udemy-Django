1ddcabb919e7aed1435018ad2861090cd3870592
Gerard Causse <grod.causse@gmail.com>
2025-04-01 20:12:25 -0400

    Complete Configuring the Admin Panel lesson

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -5,6 +5,8 @@ from .models import Author, Post, Tag
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "date", "tags")
+    list_display = ("title", "author", "date")
 
 
 # Register your models here.
