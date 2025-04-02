3c4b0b3278a7d43f02d28ffa00a7bc980722a4a8
Gerard Causse <grod.causse@gmail.com>
2025-04-01 20:07:46 -0400

    Complete Adding Data via Admin lesson

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -4,10 +4,10 @@ from .models import Author, Post, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title")}
+    prepopulated_fields = {"slug": ("title",)}
 
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Tag)
+admin.site.register(model_or_iterable=Author)
+admin.site.register(model_or_iterable=Post, admin_class=PostAdmin)
+admin.site.register(model_or_iterable=Tag)
