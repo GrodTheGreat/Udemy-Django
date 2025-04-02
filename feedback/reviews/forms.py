1f5093d461d2f60f1c7b8262d78d6b958b43fff4
Gerard Causse <grod.causse@gmail.com>
2025-04-02 19:16:38 -0400

    Complete Introducing Modelforms lesson

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,16 +1,26 @@
 from django import forms
 
+from .models import Review
 
-class ReviewForm(forms.Form):
-    name = forms.CharField(
-        label="Your Name",
-        max_length=100,
-        error_messages={
-            "required": "Your name must not be empty!",
-            "max_length": "Please enter a shorter name!",
-        },
-    )
-    review_text = forms.CharField(
-        label="Your Feedback", widget=forms.Textarea, max_length=200
-    )
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+# * Basic Form
+# class ReviewForm(forms.Form):
+#     name = forms.CharField(
+#         label="Your Name",
+#         max_length=100,
+#         error_messages={
+#             "required": "Your name must not be empty!",
+#             "max_length": "Please enter a shorter name!",
+#         },
+#     )
+#     review_text = forms.CharField(
+#         label="Your Feedback", widget=forms.Textarea, max_length=200
+#     )
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+
+
+# * ModelForm
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"  # Alternatively you can give a list of fields from the model
+        # exclude = []  # You can also do this instead
