9a6ebb500487ee1f0365ae7e9d19f0f8cd270969
Gerard Causse <grod.causse@gmail.com>
2025-04-04 17:09:01 -0400

    Complete Converting Views to Class Based lesson

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,4 +24,4 @@ from django.urls import include, path
 urlpatterns = [
     path(route="admin/", view=admin.site.urls),
     path(route="", view=include("blog.urls")),
-]
+] + static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
