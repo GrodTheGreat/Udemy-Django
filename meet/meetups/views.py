e49e16b1450dc87dd8b6c5b1a30ee24b3e45db3f
Gerard Causse <grod.causse@gmail.com>
2025-04-10 19:17:24 -0400

    Complete Querying Data lesson

--- meet/meetups/views.py
+++ meet/meetups/views.py
@@ -1,36 +1,33 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Meetup
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    meetups = [
-        {
-            "title": "A First Meetup",
-            "location": "New York",
-            "slug": "a-first-meetup",
-        },
-        {
-            "title": "A Second Meetup",
-            "location": "Paris",
-            "slug": "a-second-meetup",
-        },
-    ]
-    context = {"meetups": meetups, "show_meetups": True}
+    meetups = Meetup.objects.all()
+    context = {
+        "show_meetups": True,
+        "meetups": meetups,
+    }
     return render(
         request=request, template_name="meetups/index.html", context=context
     )
 
 
 def meetup_details(request: HttpRequest, slug: str) -> HttpResponse:
-    selected_meetup = {
-        "title": "A First Meetup",
-        "location": "New York",
-        "slug": "a-first-meetup",
-        "description": "This is the first meetup!",
-    }
-    context = {"meetup": selected_meetup}
-    return render(
-        request=request,
-        template_name="meetups/meetup-details.html",
-        context=context,
-    )
+    try:
+        selected_meetup = Meetup.objects.get(slug=slug)
+        context = {"meetup": selected_meetup}
+        return render(
+            request=request,
+            template_name="meetups/meetup-details.html",
+            context=context,
+        )
+    except Exception as exc:
+        context = {"fail": True}
+        return render(
+            request=request,
+            template_name="meetups/meetup-details.html",
+            context=context,
+        )
