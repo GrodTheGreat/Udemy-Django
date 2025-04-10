655b8201b139cd100ab3dba642f56d0654bdee88
Gerard Causse <grod.causse@gmail.com>
2025-04-10 19:52:23 -0400

    Complete Setting one-to-many Relations lesson

--- meet/meetups/admin.py
+++ meet/meetups/admin.py
@@ -5,8 +5,9 @@ from . import models
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ("title", "slug")
-    list_filter = ("title",)
+    list_filter = ("location",)
     prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(model_or_iterable=models.Meetup, admin_class=MeetupAdmin)
+admin.site.register(model_or_iterable=models.Location)
