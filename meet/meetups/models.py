6bc797fb2116ce04d26ad018064025d212f5bdfa
Gerard Causse <grod.causse@gmail.com>
2025-04-10 19:28:26 -0400

    Complete Adding Image Upload lesson

--- meet/meetups/models.py
+++ meet/meetups/models.py
@@ -5,3 +5,4 @@ class Meetup(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to="images")
