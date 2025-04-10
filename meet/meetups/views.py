95eb26baec9dcdad0a175e54f56aa4415d15f0fc
Gerard Causse <grod.causse@gmail.com>
2025-04-10 16:48:43 -0400

    Complete Getting Started with Templates lesson

--- meet/meetups/views.py
+++ meet/meetups/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request: HttpRequest):
-    return HttpResponse(content="Hello, World!")
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="meetups/index.html")
