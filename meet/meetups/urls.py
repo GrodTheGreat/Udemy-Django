b1869a6c555d9aa532e3dd5b0bafc0f94d41703e
Gerard Causse <grod.causse@gmail.com>
2025-04-10 20:40:48 -0400

    Complete More on Form Submission & Validation lesson

--- meet/meetups/urls.py
+++ meet/meetups/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path(route="", view=views.index, name="meetup-list"),
+    path(
+        route="success",
+        view=views.confirm_registration,
+        name="confirm-registration",
+    ),
     path(route="<slug:slug>", view=views.meetup_details, name="meetup-details"),
 ]
--- meet/meetups/views.py
+++ meet/meetups/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import RegistrationForm
 from .models import Meetup
@@ -18,8 +18,8 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def meetup_details(request: HttpRequest, slug: str) -> HttpResponse:
     try:
+        selected_meetup = Meetup.objects.get(slug=slug)
         if request.method == "GET":
-            selected_meetup = Meetup.objects.get(slug=slug)
             registration_form = RegistrationForm()
             context = {
                 "meetup": selected_meetup,
@@ -36,17 +36,29 @@ def meetup_details(request: HttpRequest, slug: str) -> HttpResponse:
             if registration_form.is_valid():
                 participant = registration_form.save()
                 selected_meetup.participants.add(participant)
-
+                return redirect(to="confirm-registration")
             else:
+                context = {
+                    "meetup": selected_meetup,
+                    "found": True,
+                    "form": registration_form,
+                }
                 return render(
                     request=request,
                     template_name="meetups/meetup-details.html",
                     context=context,
                 )
-    except Exception:
+    except Exception as e:
+        print(e)
         context = {"found": False}
         return render(
             request=request,
             template_name="meetups/meetup-details.html",
             context=context,
         )
+
+
+def confirm_registration(request: HttpRequest) -> HttpResponse:
+    return render(
+        request=request, template_name="meetups/registration-success.html"
+    )
